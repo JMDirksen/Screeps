fc21771728908984d7c33209fbcf20b58a886370
JMDirksen <jeftadirksen@gmail.com>
2025-04-17 07:55:35 +0200

    Sign controllers

--- dev/ToDo.js
+++ dev/ToDo.js
@@ -7,7 +7,9 @@
 [ ] Remote harvest multiple rooms
 [ ] Spawn remote harvester only when there is energy storage capacity available
 [ ] Spawn remote harvester only when there are no enemies in source/remote rooms
+[ ] Spawn claimers only when there is no danger in target room
 [ ] Attackers squads
 [ ] Ranged attackers
+[x] Upgrader: (un)sign controller
 
 */
--- dev/upgrader.js
+++ dev/upgrader.js
@@ -1,30 +1,40 @@
 module.exports = function () {
     for (const creepName in Game.creeps) {
-        const creep = Game.creeps[creepName];
-        if(creep.memory.type == 'upgrader') run(creep);
+        const creep = Game.creeps[creepName]
+        if (creep.memory.type == 'upgrader') run(creep)
     }
 };
 
 function run(creep) {
- 
+
     // Check if empty/full
-    if(creep.memory.upgrade && creep.isEmpty()) {
-        creep.memory.upgrade = false;
+    if (creep.memory.upgrade && creep.isEmpty()) {
+        creep.memory.upgrade = false
     }
-    if(!creep.memory.upgrade && creep.isFull()) {
-        creep.memory.upgrade = true;
+    if (!creep.memory.upgrade && creep.isFull()) {
+        creep.memory.upgrade = true
     }
-    
+
     // Upgrade
     if (creep.memory.upgrade) {
-        if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE)
-            creep.goTo(creep.room.controller, 3);
+        const controller = creep.room.controller
+
+        // Sign controller
+        const sign = 'Mine mine mine...'
+        if (!controller.sign || controller.sign.username != creep.owner.username || controller.sign.text != sign) {
+            if (creep.signController(controller, sign) == ERR_NOT_IN_RANGE)
+                return creep.goTo(controller, 1)
+        }
+
+        // Upgrade controller
+        if (creep.upgradeController(controller) == ERR_NOT_IN_RANGE)
+            return creep.goTo(controller, 3)
     }
-    
+
     // Get energy
-    else if(!creep.getEnergy()) {
-        if(creep.store[RESOURCE_ENERGY]) creep.memory.build = true;
-        creep.idle();
+    else if (!creep.getEnergy()) {
+        if (creep.store[RESOURCE_ENERGY]) creep.memory.build = true
+        return creep.idle()
     }
 
 }
