3314d312553406c67bd326bb962b884b10404410
JMDirksen <jeftadirksen@gmail.com>
2025-04-18 07:57:54 +0200

    Builder prioritize site with most progress

--- dev/builder.js
+++ dev/builder.js
@@ -1,38 +1,40 @@
 module.exports = function () {
     for (const creepName in Game.creeps) {
         const creep = Game.creeps[creepName];
-        if(creep.memory.type == 'builder') run(creep);
+        if (creep.memory.type == 'builder') run(creep);
     }
 };
 
 function run(creep) {
     // Switch room
-    if(creep.switchRoom()) {
+    if (creep.switchRoom()) {
         return;
     }
 
     // Check if empty/full
-    if(creep.memory.build && creep.isEmpty()) {
+    if (creep.memory.build && creep.isEmpty()) {
         creep.memory.build = false;
     }
-    if(!creep.memory.build && creep.isFull()) {
+    if (!creep.memory.build && creep.isFull()) {
         creep.memory.build = true;
     }
-    
+
     // Build
     if (creep.memory.build) {
-        const site = creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
-        if(site) {
-            if (creep.build(site) == ERR_NOT_IN_RANGE) creep.goTo(site, 3);
+        const sites = creep.room.find(FIND_MY_CONSTRUCTION_SITES)
+        if (!sites.length) return creep.idle()
+        const site = _.sortByOrder(sites, ['progress'], ['desc'])[0]
+        if (site) {
+            if (creep.build(site) == ERR_NOT_IN_RANGE) creep.goTo(site, 3)
         }
         else {
-            creep.idle();
+            creep.idle()
         }
     }
-    
+
     // Get energy
-    else if(!creep.getEnergy()) {
-        if(creep.store[RESOURCE_ENERGY]) creep.memory.build = true;
+    else if (!creep.getEnergy()) {
+        if (creep.store[RESOURCE_ENERGY]) creep.memory.build = true;
         creep.idle();
     }
 
