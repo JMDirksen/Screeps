ee4f91cde8ecd907bd4475d18b26384358cb61ea
JMDirksen <jeftadirksen@gmail.com>
2025-04-19 12:15:56 +0200

    info/debug room links

--- dev/functions.js
+++ dev/functions.js
@@ -3,11 +3,13 @@ if (Memory.debug == undefined) Memory.debug = false
 
 global.info = function (text) {
     if (typeof text === 'object') text = JSON.stringify(text)
+    text = text.replace(/(?:W|E)\d{1,2}(?:N|S)\d{1,2}/gi, '<a href="#!/room/$&">$&</a>')
     if (Memory.info) console.log('<font color="Cyan">Info: </font>' + text)
 }
 
 global.debug = function (text) {
     if (typeof text === 'object') text = JSON.stringify(text)
+    text = text.replace(/(?:W|E)\d{1,2}(?:N|S)\d{1,2}/gi, '<a href="#!/room/$&">$&</a>')
     if (Memory.debug) console.log('<font color="DarkGoldenrod">Debug: </font>' + text)
 }
 
