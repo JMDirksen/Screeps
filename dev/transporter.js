5eec8a50dc5b7149cebda4586f55dc23cca9be0e
JMDirksen <jeftadirksen@gmail.com>
2025-04-22 21:01:39 +0200

    Fill before idle

--- dev/transporter.js
+++ dev/transporter.js
@@ -1,6 +1,6 @@
 module.exports = function () {
     for (const creepName in Game.creeps) {
-        const creep = Game.creeps[creepName];
+        const creep = Game.creeps[creepName]
         if (creep.memory.type == 'transporter') run(creep)
     }
 };
@@ -14,10 +14,10 @@ function run(creep) {
 
     // Check if empty/full
     if (creep.memory.transport && creep.isEmpty()) {
-        creep.memory.transport = false;
+        creep.memory.transport = false
     }
     if (!creep.memory.transport && creep.isFull()) {
-        creep.memory.transport = true;
+        creep.memory.transport = true
     }
 
     // Emergency tower supply
@@ -45,11 +45,11 @@ function run(creep) {
         filter: s =>
             s.structureType.isInList(STRUCTURE_SPAWN, STRUCTURE_EXTENSION)
             && s.store.getFreeCapacity(RESOURCE_ENERGY)
-    });
+    })
     if (spawn && creep.memory.transport) {
-        const r = creep.transfer(spawn, RESOURCE_ENERGY);
-        if (r == ERR_NOT_IN_RANGE) creep.goTo(spawn, 1);
-        return;
+        const r = creep.transfer(spawn, RESOURCE_ENERGY)
+        if (r == ERR_NOT_IN_RANGE) creep.goTo(spawn, 1)
+        return
     }
     if (spawn && !creep.memory.transport) {
         if (creep.getEnergy()) return;
@@ -76,23 +76,29 @@ function run(creep) {
         filter: s =>
             s.structureType == STRUCTURE_STORAGE
             && s.store.getFreeCapacity(RESOURCE_ENERGY)
-    });
+    })
     if (storage && creep.memory.transport) {
-        const r = creep.transfer(storage, RESOURCE_ENERGY);
-        if (r == ERR_NOT_IN_RANGE) creep.goTo(storage, 1);
-        return;
+        const r = creep.transfer(storage, RESOURCE_ENERGY)
+        if (r == ERR_NOT_IN_RANGE) creep.goTo(storage, 1)
+        return
     }
     if (storage && !creep.memory.transport) {
-        if (creep.getEnergy({ fromStorage: false })) return;
+        if (creep.getEnergy({ fromStorage: false })) return
     }
 
     // Switch to transporting if no energy to collect
     if (!creep.memory.transport && creep.store.getUsedCapacity(RESOURCE_ENERGY)) {
-        creep.memory.transport = true;
-        return;
+        creep.memory.transport = true
+        return
+    }
+
+    // Fill before idle
+    if (creep.memory.transport && !creep.isFull()) {
+        creep.memory.transport = false
+        if (creep.getEnergy()) return
     }
 
     // Idle
-    creep.idle();
+    creep.idle()
 
 }
