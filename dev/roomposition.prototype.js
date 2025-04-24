c1fd35d7c0c23d4d39a55c3cc74644876af335ed
JMDirksen <jeftadirksen@gmail.com>
2025-04-24 07:36:14 +0200

    Refactor: getPosInDirection

--- dev/roomposition.prototype.js
+++ dev/roomposition.prototype.js
@@ -4,24 +4,13 @@ RoomPosition.prototype.lookForInDirection = function (direction, type) {
 
 RoomPosition.prototype.getPosInDirection = function (direction) {
     let rp = new RoomPosition(this.x, this.y, this.roomName)
-    switch (direction) {
-        case TOP:
-            return rp.y -= 1, rp
-        case TOP_RIGHT:
-            return rp.x += 1, rp.y -= 1, rp
-        case RIGHT:
-            return rp.x += 1, rp
-        case BOTTOM_RIGHT:
-            return rp.x += 1, rp.y += 1, rp
-        case BOTTOM:
-            return rp.y += 1, rp
-        case BOTTOM_LEFT:
-            return rp.x -= 1, rp.y += 1, rp
-        case LEFT:
-            return rp.x -= 1, rp
-        case TOP_LEFT:
-            return rp.x -= 1, rp.y -= 1, rp
-        default:
-            return false
-    }
+    if (direction == LEFT)         return rp.x -= 1, rp
+    if (direction == RIGHT)        return rp.x += 1, rp
+    if (direction == TOP)          return rp.y -= 1, rp
+    if (direction == BOTTOM)       return rp.y += 1, rp
+    if (direction == TOP_LEFT)     return rp.x -= 1, rp.y -= 1, rp
+    if (direction == BOTTOM_RIGHT) return rp.x += 1, rp.y += 1, rp
+    if (direction == BOTTOM_LEFT)  return rp.x -= 1, rp.y += 1, rp
+    if (direction == TOP_RIGHT)    return rp.x += 1, rp.y -= 1, rp
+    return false
 }
