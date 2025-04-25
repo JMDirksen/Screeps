9978b3b9c9b1b0392af10a571a36aba535d3e5a7
JMDirksen <jeftadirksen@gmail.com>
2025-04-25 08:00:17 +0200

    fix: repairer in getEnergy job doesn't idle

--- dev/repairer.js
+++ dev/repairer.js
@@ -1,17 +1,17 @@
 module.exports = function () {
     for (const creepName in Game.creeps) {
-        const creep = Game.creeps[creepName];
-        if (creep.memory.type == 'repairer') run(creep);
+        const creep = Game.creeps[creepName]
+        if (creep.memory.type == 'repairer') run(creep)
     }
-};
+}
 
 function run(creep) {
     // Flee
-    if(creep.flee()) return
+    if (creep.flee()) return
 
     // Switch room
     if (creep.switchRoom()) {
-        return;
+        return
     }
 
     // Get job
@@ -22,25 +22,23 @@ function run(creep) {
 
     // Get energy
     if (creep.memory.job == 'getEnergy') {
-        if (creep.isFull()) creep.memory.job = getRepairJob(creep);
-        else return creep.getEnergy();
+        if (creep.isFull()) creep.memory.job = getRepairJob(creep)
+        else if (creep.getEnergy()) return
+        else return creep.idle()
     }
 
     // Do job
-    let job = Game.getObjectById(creep.memory.job);
-    let r = creep.repair(job);
+    let job = Game.getObjectById(creep.memory.job)
+    let r = creep.repair(job)
     if (r == OK) {
-        if (job.hits == job.hitsMax) creep.memory.job = getRepairJob(creep);
-        return;
+        if (job.hits == job.hitsMax) creep.memory.job = getRepairJob(creep)
     }
     else if (r == ERR_NOT_IN_RANGE) {
-        creep.goTo(job, 3);
-        return;
+        creep.goTo(job, 3)
     }
     else if (r == ERR_NOT_ENOUGH_RESOURCES) {
-        creep.memory.job = 'getEnergy';
-        creep.getEnergy();
-        return;
+        creep.memory.job = 'getEnergy'
+        creep.getEnergy()
     }
     else {
         creep.memory.job = false
@@ -53,8 +51,8 @@ function getRepairJob(creep) {
         filter: s =>
             s.structureType.isInList(STRUCTURE_ROAD, STRUCTURE_CONTAINER, STRUCTURE_STORAGE, STRUCTURE_LINK, STRUCTURE_TOWER)
             && s.hits < s.hitsMax
-    });
-    const structure = _.sortBy(structures, 'hits')[0];
-    if (structure) return structure.id;
-    else return false;
+    })
+    const structure = _.sortBy(structures, 'hits')[0]
+    if (structure) return structure.id
+    else return false
 }
