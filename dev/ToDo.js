29732e79b3b03e826277ffc536baf97f4c6bf139
JMDirksen <jeftadirksen@gmail.com>
2025-04-29 15:51:33 +0200

    fix: transporter to links

--- dev/ToDo.js
+++ dev/ToDo.js
@@ -2,8 +2,6 @@
 
 [.] Attackers (room)
 [ ] Market code (terminal required)
-[x] Info/Debug show tick
 [ ] Tier 5 creeps
-[x] Auto road build
 
 */
--- dev/store.prototype.js
+++ dev/store.prototype.js
@@ -1,5 +1,5 @@
 'use strict'
 
-Store.prototype.getUsedPercentage = function (resource = null) {
+Store.prototype.getUsedPercentage = function (resource = RESOURCE_ENERGY) {
     return Math.round(this.getUsedCapacity(resource) / this.getCapacity(resource) * 100)
 }
