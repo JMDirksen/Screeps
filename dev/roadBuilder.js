eb5e2e55a3d1daef0f87dc7ba205c12d18fb7353
JMDirksen <jeftadirksen@gmail.com>
2025-04-29 16:11:54 +0200

    tweak: roadBuilder aggresiveness

--- dev/roadBuilder.js
+++ dev/roadBuilder.js
@@ -1,17 +1,17 @@
 'use strict'
 
 module.exports = function () {
-    if (Game.time % 25) return
+    if (Game.time % 100) return
 
     for (const spawnName in Game.spawns) {
         const spawn = Game.spawns[spawnName]
         let list = spawn.memory.autoBuildRoadsList
-        //debug(list)
-
+        
         // Look for road to build
         const highestCount = _.sortByOrder(list, ['count'], ['desc'])[0]
+        debug(`${spawn.room.name} roadBuilder: ${JSON.stringify(list)}`)
         // Build road
-        if (highestCount && highestCount.count >= 10) {
+        if (highestCount && highestCount.count >= 5) {
             let roomPos = new RoomPosition(highestCount.x, highestCount.y, spawn.room.name)
             roomPos.createConstructionSite(STRUCTURE_ROAD)
             _.remove(list, i => i.x == highestCount.x && i.y == highestCount.y)
