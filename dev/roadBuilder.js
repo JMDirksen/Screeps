96fef8cdd728b78893f11fdd3d44e961044786fc
JMDirksen <jeftadirksen@gmail.com>
2025-04-29 15:50:52 +0200

    fix: count of undefined

--- dev/roadBuilder.js
+++ dev/roadBuilder.js
@@ -11,7 +11,7 @@ module.exports = function () {
         // Look for road to build
         const highestCount = _.sortByOrder(list, ['count'], ['desc'])[0]
         // Build road
-        if (highestCount.count >= 10) {
+        if (highestCount && highestCount.count >= 10) {
             let roomPos = new RoomPosition(highestCount.x, highestCount.y, spawn.room.name)
             roomPos.createConstructionSite(STRUCTURE_ROAD)
             _.remove(list, i => i.x == highestCount.x && i.y == highestCount.y)
