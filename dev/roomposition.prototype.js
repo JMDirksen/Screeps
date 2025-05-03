8dd7ce1a86a4201ee91db7f420f6bd434b3397f0
JMDirksen <jeftadirksen@gmail.com>
2025-05-03 09:54:24 +0200

    fix: Error: Invalid coordinate

--- dev/roomposition.prototype.js
+++ dev/roomposition.prototype.js
@@ -6,14 +6,18 @@ RoomPosition.prototype.lookForInDirection = function (direction, type) {
 
 RoomPosition.prototype.getPosInDirection = function (direction) {
     let rp = new RoomPosition(this.x, this.y, this.roomName)
-    if (direction == LEFT)         return rp.x -= 1, rp
-    if (direction == RIGHT)        return rp.x += 1, rp
-    if (direction == TOP)          return rp.y -= 1, rp
-    if (direction == BOTTOM)       return rp.y += 1, rp
-    if (direction == TOP_LEFT)     return rp.x -= 1, rp.y -= 1, rp
-    if (direction == BOTTOM_RIGHT) return rp.x += 1, rp.y += 1, rp
-    if (direction == BOTTOM_LEFT)  return rp.x -= 1, rp.y += 1, rp
-    if (direction == TOP_RIGHT)    return rp.x += 1, rp.y -= 1, rp
+    try {
+        if (direction == LEFT)         return rp.x -= 1, rp
+        if (direction == RIGHT)        return rp.x += 1, rp
+        if (direction == TOP)          return rp.y -= 1, rp
+        if (direction == BOTTOM)       return rp.y += 1, rp
+        if (direction == TOP_LEFT)     return rp.x -= 1, rp.y -= 1, rp
+        if (direction == BOTTOM_RIGHT) return rp.x += 1, rp.y += 1, rp
+        if (direction == BOTTOM_LEFT)  return rp.x -= 1, rp.y += 1, rp
+        if (direction == TOP_RIGHT)    return rp.x += 1, rp.y -= 1, rp
+    } catch (error) {
+        return false
+    }
     return false
 }
 
