173d3d9ed7be21838f857c3803ea17a9c59b8aeb
JMDirksen <jeftadirksen@gmail.com>
2025-05-18 09:44:01 +0200

    Terminal sell to buy order

--- dev/s.terminal.js
+++ dev/s.terminal.js
@@ -1,32 +1,29 @@
 'use strict'
 
 module.exports = function () {
-    if (Game.time % 25) return
+    if (Game.time % 100) return
     const terminals = _.filter(Game.structures, { structureType: STRUCTURE_TERMINAL })
     for (const terminal of terminals) {
         const roomName = terminal.room.name
         const roomMineral = terminal.room.find(FIND_MINERALS)[0]
         const roomMineralType = roomMineral ? roomMineral.mineralType : null
 
-        // Create sell order
+        // Sell to buy order
         if (roomMineralType) {
-            // Check if order not exists
-            const existingOrder = _.filter(Game.market.orders, {
-                roomName: roomName, type: 'sell', resourceType: roomMineralType
-            })[0]
-            //debug(`existingOrder: ${JSON.stringify(existingOrder)}`)
-            if (!existingOrder) {
-                let r = Game.market.createOrder({
-                    type: ORDER_SELL,
-                    resourceType: roomMineralType,
-                    price: 1,
-                    totalAmount: 1,
-                    roomName: roomName
-                })
-                if (r == OK) info(`Sell order created`)
-                else if (r == ERR_NOT_ENOUGH_RESOURCES) info(`Not enough credits to create order`)
-                else info(`Create sell order failed: ${r}`)
+            const buyOrder = _.sortByOrder(Game.market.getAllOrders({
+                type: ORDER_BUY, resourceType: roomMineralType
+            }), ['price'], ['desc'])[0]
+            debug(`buyOrder: ${JSON.stringify(buyOrder)}`)
+            if (buyOrder) {
+                const amount = Math.min(buyOrder.amount, terminal.store.getUsedCapacity(roomMineralType))
+                const cost = Game.market.calcTransactionCost(amount, roomName, buyOrder.roomName)
+                debug(cost)
+                let r = Game.market.deal(buyOrder.id, amount, roomName)
+                if (r == OK) info(`Sold to buy order`)
+                else if (r == ERR_NOT_ENOUGH_RESOURCES) info(`Not enough resources to sell (cost: ${cost})`)
+                else info(`Sell to buy order failed: ${r}`)
             }
         }
+
     }
 }
